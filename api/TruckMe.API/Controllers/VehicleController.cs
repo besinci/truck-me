087c881c3e5518140b71f35503f689c20a032bfe
Ali Besinci <aliosmanbesinci@gmail.com>
2023-05-29 23:15:03 +0300

    initial project

---
+++ api/TruckMe.API/Controllers/VehicleController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TruckMe.API.Models;
+
+namespace TruckMe.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VehicleController : ControllerBase
+{
+    private static readonly List<Brand> brands = new List<Brand>()
+    {
+        new Brand() { Id = 1, Name = "Honda"},
+        new Brand() { Id = 2, Name = "BMW Motorrad"},
+    };
+
+    private static readonly List<Vehicle> _vehicles = new List<Vehicle>()
+    {
+        new Vehicle() { Id = 1, Brand = brands[0], Model = "CRF 250L"},
+        new Vehicle() { Id = 2, Brand = brands[0], Model = "NC 750X"},
+        new Vehicle() { Id = 3, Brand = brands[1], Model = "1250 GS"},
+        new Vehicle() { Id = 4, Brand = brands[0], Model = "Transalp"}
+    };
+
+    [HttpGet(Name = "GetVehicles")]
+    public IEnumerable<Vehicle> Get()
+    {
+        return _vehicles;
+    }
+
+}
+
---
+++ api/TruckMe.API/Models/DataModels.cs
@@ -0,0 +1,22 @@
+﻿namespace TruckMe.API.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public required Brand Brand { get; set; }
+        public required string Model { get; set; }
+    }
+
+    public class Brand
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+
+    public class UserVehicle
+    {
+        public int Id { get; set; }
+        public List<Vehicle>? Vehicles { get; set; }
+        public required string UserName { get; set; }
+    }
+}
